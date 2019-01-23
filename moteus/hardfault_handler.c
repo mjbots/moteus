79ae4fc8e104ca6acc3ae2b9fa60495b9e6ea442
Josh Pieper <jjp@pobox.com>
2019-01-22 20:08:49 -0500

    Remove "all rights reserved" from all the copyright notices
    They are all licensed under the Apache 2.0 license, so it was kind of
    pointless.

--- moteus/hardfault_handler.c
+++ moteus/hardfault_handler.c
@@ -1,4 +1,4 @@
-// Copyright 2015-2018 Josh Pieper, jjp@pobox.com.  All rights reserved.
+// Copyright 2015-2018 Josh Pieper, jjp@pobox.com.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
