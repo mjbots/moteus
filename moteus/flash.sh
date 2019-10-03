0ddc9c8b251de3d38dd38479c191821ce58713c1
Josh Pieper <jjp@pobox.com>
2019-10-03 14:07:49 -0400

    Make the openocd scripts work for more openocd versions

--- moteus/flash.sh
+++ moteus/flash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 openocd \
-    -f /usr/share/openocd/scripts/interface/stlink-v2.cfg \
-    -f /usr/share/openocd/scripts/target/stm32f4x.cfg \
+    -f interface/stlink.cfg \
+    -f target/stm32f4x.cfg \
     -c "init" \
     -c "reset_config none separate; program bazel-out/stm32f4-opt/bin/moteus/moteus.08000000.bin verify 0x8000000; program bazel-out/stm32f4-opt/bin/moteus/bootloader.0800c000.bin verify 0x800c000; program bazel-out/stm32f4-opt/bin/moteus/moteus.08010000.bin verify reset exit 0x8010000"
