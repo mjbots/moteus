b839e31b2db97e88975380fe73b686ea5b7b591a
Josh Pieper <jjp@pobox.com>
2020-01-15 11:37:57 -0500

    Add a script which sets the nSWBOOT0 option bit

---
+++ moteus/program_option_bytes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This clears the nSWBOOT0 option byte, which forces the device to
+# start executing from user code regardless of the state of the PB8
+# pin.
+
+openocd \
+    -f interface/stlink.cfg \
+    -f target/stm32g4x.cfg \
+    -c "init" \
+    -c "stm32l4x option_write 0 0x20 0x00000000 0x04000000" \
+    -c "stm32l4x option_load 0"
