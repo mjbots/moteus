e823fe7f95f10553df73741a8eb6679852e4216c
Josh Pieper <jjp@pobox.com>
2020-06-19 13:23:08 -0400

    Add a firmware release script

---
+++ make_release.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+# Copyright 2020 Josh Pieper, jjp@pobox.com.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import subprocess
+import sys
+
+
+def run(cmd):
+    print('>{}'.format(cmd))
+    subprocess.check_call(cmd, shell=True)
+
+def main():
+    outdir = sys.argv[1]
+
+    # Make sure git is clean first.
+    dirty = (subprocess.run("git diff-index --quiet HEAD --",
+                            shell=True).returncode != 0)
+    if dirty:
+        raise RuntimeError("git is dirty, cannot release!")
+    git_hash = subprocess.check_output(
+        "git rev-parse HEAD", shell=True).decode('utf8').strip()
+
+    datestr = datetime.datetime.now().strftime('%Y%m%d')
+
+    print("Details:")
+    print(" Date:", datestr)
+    print(" Output Directory:", outdir)
+    print(" Git Hash:", git_hash)
+    print("Building...")
+    print()
+
+    run('tools/bazel clean --expunge')
+    run('tools/bazel build --cpu stm32g4 -c opt //:target')
+
+    run(f'cp bazel-bin/moteus/moteus.elf {outdir}/{datestr}-moteus-{git_hash}.elf')
+    run(f'cp bazel-bin/moteus/can_bootloader.elf {outdir}/{datestr}-bootloader-{git_hash}.elf')
+    run(f'cp bazel-bin/moteus/power_dist.elf {outdir}/{datestr}-power_dist-{git_hash}.elf')
+
+    print()
+    print('DONE')
+
+if __name__ == '__main__':
+    main()
