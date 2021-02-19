bc27a2dfe0355147243d2b2d69b7e02108709046
Josh Pieper <jjp@pobox.com>
2021-02-19 06:09:59 -0500

    Add a torque vs speed plot for the qdd100 beta2

---
+++ qualification/qdd100_torque_vs_speed.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# Copyright 2021 Josh Pieper, jjp@pobox.com.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+def main():
+    plt.plot([0, 1300, 3700],
+             [16, 16, 0])
+    plt.xlabel('Speed (dps)')
+    plt.ylabel('Torque (Nm)')
+    plt.grid()
+    plt.title('qdd100 beta 2 Peak Torque vs Speed at 36V')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
