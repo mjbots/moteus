mjbots/moteus

commit 5e3d7f1997dc33627ff8876908c39a6f24705926

Switch unwrapped_position_raw to be 64 bits
This is now in the same units as control_position.  This is to
eventually support encoder filtering, for which more resolution will
be desired.

We also update mjlib to get better error messages.

fw/bldc_servo.cc
@@ -859,13 +859,15 @@ class BldcServo::Impl {
       const float integral_offsets =
           std::round(error / motor_.unwrapped_position_scale);
       status_.unwrapped_position_raw =
+          static_cast<int64_t>(65536ll * 65536ll) *
           zero_position + integral_offsets * 65536.0f;
       status_.position_to_set = std::numeric_limits<float>::quiet_NaN();
       if (need_rezero_because_init) {
         status_.rezeroed = true;
       }
     } else {
-      status_.unwrapped_position_raw += delta_position;
+      status_.unwrapped_position_raw +=
+          static_cast<int64_t>(65536ll * 65536ll) * delta_position;
     }
 
     {
@@ -881,7 +883,7 @@ class BldcServo::Impl {
     }
 
     status_.unwrapped_position =
-        status_.unwrapped_position_raw / motor_scale16_;
+        (status_.unwrapped_position_raw >> 32) / motor_scale16_;
 
     // The temperature sensing should be done by now, but just double
     // check.
@@ -1192,6 +1194,7 @@ class BldcServo::Impl {
 
     if (data->set_position) {
       status_.unwrapped_position_raw =
+          static_cast<int64_t>(65536ll * 65536ll) *
           static_cast<int32_t>(*data->set_position * 65536.0f);
       data->set_position = {};
     }
@@ -1564,11 +1567,6 @@ class BldcServo::Impl {
       float max_torque_Nm,
       float feedforward_Nm,
       float velocity) MOTEUS_CCM_ATTRIBUTE {
-    // Note that status_.control_position is measured in terms of 1 /
-    // 2**32th of unwrapped_position_raw.  This is so that velocities
-    // do not become so small as to result in no change whatsoever at
-    // the control rate.
-    //
     // We go to some lengths in our conversions to and from
     // control_position so as to avoid converting a float directly to
     // an int64, which calls out to a system library that is pretty
@@ -1581,9 +1579,7 @@ class BldcServo::Impl {
               static_cast<int32_t>(motor_scale16_ * data->position));
       data->position = std::numeric_limits<float>::quiet_NaN();
     } else if (!status_.control_position) {
-      status_.control_position =
-          static_cast<int64_t>(65536ll * 65536ll) *
-          static_cast<int64_t>(status_.unwrapped_position_raw);
+      status_.control_position = status_.unwrapped_position_raw;
     }
 
     auto velocity_command = velocity;
@@ -1598,20 +1594,18 @@ class BldcServo::Impl {
              kRateHz));
 
     if (std::isfinite(config_.max_position_slip)) {
-      const int32_t current_position = status_.unwrapped_position_raw;
-      const int32_t slip =
+      const int64_t current_position = status_.unwrapped_position_raw;
+      const int64_t slip =
+          static_cast<int64_t>(65536ll * 65536ll) *
           static_cast<int32_t>(motor_scale16_ * config_.max_position_slip);
 
-      const int32_t error =
-          current_position - static_cast<int32_t>(
-              *status_.control_position / (65536ll * 65536ll));
+      const int64_t error =
+          current_position - *status_.control_position;
       if (error < -slip) {
-        *status_.control_position = static_cast<int64_t>(65536ll * 65536ll) *
-            (current_position + slip);
+        *status_.control_position = current_position + slip;
       }
       if (error > slip) {
-        *status_.control_position = static_cast<int64_t>(65536ll * 65536ll) *
-            (current_position - slip);
+        *status_.control_position = current_position - slip;
       }
     }
 
@@ -1664,11 +1658,11 @@ class BldcServo::Impl {
     // We always control relative to the control position of 0, so
     // that we get equal performance across the entire viable integral
     // position range.
-    const int32_t scaled_control =
-        static_cast<int32_t>(*status_.control_position / (65536ll * 65536ll));
     const float unlimited_torque_Nm =
         pid_position_.Apply(
-            (status_.unwrapped_position_raw - scaled_control) /
+            static_cast<int32_t>(
+                (status_.unwrapped_position_raw -
+                 *status_.control_position) >> 32) /
             65536.0f * motor_.unwrapped_position_scale,
             0.0,
             measured_velocity, velocity_command,

utils/dynamometer_drive.cc
@@ -1594,7 +1594,7 @@ class Application {
 
       const auto position_error =
           (*spinning_dut.control_position / (65536ll * 65536ll)) -
-          spinning_dut.unwrapped_position_raw;
+          spinning_dut.unwrapped_position_raw / (65536ll * 65536ll);
       if (std::abs(position_error) > 4000) {
         throw mjlib::base::system_error::einval(
             fmt::format("Base tracking not working |{}| > 4000",

fw/bldc_servo.h
@@ -345,7 +345,10 @@ class BldcServo {
     float d_A = 0.0f;
     float q_A = 0.0f;
 
-    int32_t unwrapped_position_raw = 0;
+    // This is a fixed point number, with the upper 32 bits being
+    // measured in encoder ticks, and the bottom 32 bits being
+    // fractional.
+    int64_t unwrapped_position_raw = 0;
     float unwrapped_position = 0.0f;
     float velocity = 0.0f;
     float torque_Nm = 0.0f;
@@ -354,7 +357,7 @@ class BldcServo {
     SimplePI::State pid_q;
     PID::State pid_position;
 
-    // This is scaled to be 65536 larger than unwrapped_position_raw.
+    // This is measured in the same units as unwrapped_position_raw.
     std::optional<int64_t> control_position;
     float position_to_set = 0.0;
     float timeout_s = 0.0;

tools/workspace/mjlib/repository.bzl
@@ -20,6 +20,6 @@ def mjlib_repository(name):
     github_archive(
         name = name,
         repo = "mjbots/mjlib",
-        commit = "fbfa309edcdd641ee1d04a376bb7cacf6afc0114",
-        sha256 = "ecdf134ae1bed0a4c7cf256c934f91ce2647f1109838a239aa82dd2f778a5606",
+        commit = "a414f92ea4b5c3a246074754243806b58a2e860f",
+        sha256 = "7ca9d1a39b6569beae3cb81bbb4e0f6b8ad57550599f0f635f31027ba1c1f938",
     )

utils/moteus_subset.h
@@ -47,7 +47,7 @@ struct ServoStats {
   Fault fault = kNone;
 
   uint16_t position_raw = 0;
-  int32_t unwrapped_position_raw = 0;
+  int64_t unwrapped_position_raw = 0;
   float unwrapped_position = 0.0f;
 
   float torque_Nm = 0.0f;

commit 78d4b057136704870e1979d74da08d8170e98c77

Make rezeroing to non-zero positions work again
This was broken in 5e3d7f1997dc33627ff8876908c39a6f24705926

fw/bldc_servo.cc
@@ -871,7 +871,9 @@ class BldcServo::Impl {
           std::round(error / motor_.unwrapped_position_scale);
       status_.unwrapped_position_raw =
           static_cast<int64_t>(65536ll * 65536ll) *
-          zero_position + integral_offsets * 65536.0f;
+          zero_position +
+          static_cast<int32_t>(integral_offsets * 65536.0f) *
+          65536ll * 65536ll;
       status_.position_to_set = std::numeric_limits<float>::quiet_NaN();
       // In case we are in encoder PLL mode.
       status_.velocity = 0.0f;

utils/dynamometer_drive.cc
@@ -91,6 +91,7 @@ struct Options {
   bool validate_dq_ilimit = false;
   bool validate_power_limit = false;
   bool validate_max_velocity = false;
+  bool validate_rezero = false;
 
   template <typename Archive>
   void Serialize(Archive* a) {
@@ -121,6 +122,7 @@ struct Options {
     a->Visit(MJ_NVP(validate_dq_ilimit));
     a->Visit(MJ_NVP(validate_power_limit));
     a->Visit(MJ_NVP(validate_max_velocity));
+    a->Visit(MJ_NVP(validate_rezero));
   }
 };
 
@@ -609,6 +611,8 @@ class Application {
       co_await ValidatePowerLimit();
     } else if (options_.validate_max_velocity) {
       co_await ValidateMaxVelocity();
+    } else if (options_.validate_rezero) {
+      co_await ValidateRezero();
     } else {
       fmt::print("No cycle selected\n");
     }
@@ -1936,6 +1940,32 @@ class Application {
         }
       }
     }
+
+    co_return;
+  }
+
+  boost::asio::awaitable<void> DoRezeroTest(double value) {
+    co_await dut_->Command(fmt::format("d rezero {}", value));
+    co_await Sleep(0.5);
+
+    {
+      // We should be within 0.5 of the desired.
+      const auto pos = dut_->servo_stats().unwrapped_position;
+      if (std::abs(pos - value) > 0.5) {
+        throw mjlib::base::system_error::einval(
+            fmt::format(
+                "DUT rezero != {} ({})",
+                value, pos));
+      }
+    }
+  }
+  boost::asio::awaitable<void> ValidateRezero() {
+    co_await fixture_->Command("d stop");
+    co_await dut_->Command("d stop");
+
+    co_await DoRezeroTest(0.0);
+    co_await DoRezeroTest(4.2);
+    co_await DoRezeroTest(-7.9);
 
     co_return;
   }

utils/firmware_validate.py
@@ -94,6 +94,9 @@ class TestDynoFast(unittest.TestCase):
     def test_validate_max_velocity(self):
         dyno('--validate_max_velocity', '1')
 
+    def test_rezero(self):
+        dyno('--validate_rezero', '1')
+
 
 class TestDynoSlow(unittest.TestCase):
     def test_torque_ripple(self):