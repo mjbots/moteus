337824c1f866288bc2ccb104428849110c6a4fc0
Josh Pieper <jjp@pobox.com>
2022-05-16 20:43:51 -0400

    Allow kd_scale to be configured in zero_velocity mode

--- fw/bldc_servo.cc
+++ fw/bldc_servo.cc
@@ -1766,7 +1766,7 @@ class BldcServo::Impl {
   void ISR_DoZeroVelocity(const SinCos& sin_cos, CommandData* data) MOTEUS_CCM_ATTRIBUTE {
     PID::ApplyOptions apply_options;
     apply_options.kp_scale = 0.0f;
-    apply_options.kd_scale = 1.0f;
+    apply_options.kd_scale = data->kd_scale;
     apply_options.ki_scale = 0.0f;
 
     ISR_DoPositionCommon(sin_cos, data,
