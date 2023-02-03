mjbots/moteus

commit 0d76d71be2c876c438bb19c6465721856dfff8fe

Support synchronizing the clock of a moteus controller to the host
In the firmware, this exposes two new registers.  The first reports a
counter that increments once per millisecond.  The second is a command
that allows the application to change the processor speed in 0.25%
increments.  Document these new registers and expose them to python.

Implement an example python script that shows how these registers can
be used to force a moteus controller to operate with time synchronized
to the host application.

lib/python/moteus/moteus.py
@@ -245,6 +245,9 @@ class Register(enum.IntEnum):
     AUX2_ANALOG_IN4 = 0x06b
     AUX2_ANALOG_IN5 = 0x06c
 
+    MILLISECOND_COUNTER = 0x070
+    CLOCK_TRIM = 0x071
+
     REGISTER_MAP_VERSION = 0x102
     SERIAL_NUMBER = 0x120
     SERIAL_NUMBER1 = 0x120
@@ -484,6 +487,10 @@ def parse_register(parser, register, resolution):
           register == Register.AUX2_ANALOG_IN4 or
           register == Register.AUX2_ANALOG_IN5):
         return parser.read_pwm(resolution)
+    elif register == Register.MILLISECOND_COUNTER:
+        return parser.read_int(resolution)
+    elif register == Register.CLOCK_TRIM:
+        return parser.read_int(resolution)
     else:
         # We don't know what kind of value this is, so we don't know
         # the units.
@@ -655,11 +662,6 @@ class Controller:
 
         return buf.getvalue()
 
-    def _extract(self, value):
-        if len(value):
-            return value[0]
-        return None
-
     def _make_command(self, *, query, source=0):
         result = cmd.Command()
 
@@ -677,8 +679,32 @@ class Controller:
         return result;
 
     async def query(self, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_query(**kwargs)]))
+        return await self.execute(self.make_query(**kwargs))
+
+    def make_custom_query(self, to_query_fields):
+        """Return a moteus.Command structure with data required to query the
+        registers given by the 'to_query_fields' dictionary of
+        registers to resolutions.
+        """
+
+        result = self._make_command(query=True)
+
+        buf = io.BytesIO()
+        writer = Writer(buf)
+
+        min_val = int(min(to_query_fields.keys()))
+        max_val = int(max(to_query_fields.keys()))
+        c = mp.WriteCombiner(writer, 0x10, min_val,
+                             [to_query_fields.get(i, mp.IGNORE)
+                              for i in range(min_val, max_val + 1)])
+        for _ in range(min_val, max_val + 1):
+            c.maybe_write()
+
+        result.data = buf.getvalue()
+        return result
+
+    async def custom_query(self, *args, **kwargs):
+        return await self.execute(self.make_custom_query(*args, **kwargs))
 
     def make_stop(self, *, query=False):
         """Return a moteus.Command structure with data necessary to send a
@@ -700,8 +726,7 @@ class Controller:
         return result
 
     async def set_stop(self, *args, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_stop(**kwargs)]))
+        return await self.execute(self.make_stop(**kwargs))
 
     def make_set_output(self, *,
                         position=0.0,
@@ -738,8 +763,7 @@ class Controller:
             position=position, query=query, cmd=Register.SET_OUTPUT_EXACT)
 
     async def set_output(self, *args, cmd=None, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_set_output(**kwargs, cmd=cmd)]))
+        return await self.execute(self.make_set_output(**kwargs, cmd=cmd))
 
     async def set_output_nearest(self, *args, **kwargs):
         return await self.set_output(cmd=Register.SET_OUTPUT_NEAREST, **kwargs)
@@ -757,8 +781,7 @@ class Controller:
             position=rezero, query=query, cmd=Register.SET_OUTPUT_NEAREST)
 
     async def set_rezero(self, *args, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_rezero(**kwargs)]))
+        return await self.execute(self.make_rezero(**kwargs))
 
 
     def make_position(self,
@@ -836,8 +859,7 @@ class Controller:
         return result
 
     async def set_position(self, *args, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_position(**kwargs)]))
+        return await self.execute(self.make_position(**kwargs))
 
     def make_vfoc(self,
                   *,
@@ -892,8 +914,7 @@ class Controller:
         return result
 
     async def set_vfoc(self, *args, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_vfoc(**kwargs)]))
+        return await self.execute(self.make_vfoc(**kwargs))
 
     def make_current(self,
                      *,
@@ -937,8 +958,7 @@ class Controller:
         return result
 
     async def set_current(self, *args, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_current(**kwargs)]))
+        return await self.execute(self.make_current(**kwargs))
 
     def make_stay_within(
             self,
@@ -1002,8 +1022,7 @@ class Controller:
         return result
 
     async def set_stay_within(self, *args, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_stay_within(**kwargs)]))
+        return await self.execute(self.make_stay_within(**kwargs))
 
     def make_brake(self, *, query=False):
         result = self._make_command(query=query)
@@ -1022,8 +1041,7 @@ class Controller:
         return result
 
     async def set_brake(self, *args, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_brake(**kwargs)]))
+        return await self.execute(self.make_brake(**kwargs))
 
     def make_write_gpio(self, aux1=None, aux2=None, query=False):
         """Return a moteus.Command structure with data necessary to set one or
@@ -1056,8 +1074,7 @@ class Controller:
         return result
 
     async def set_write_gpio(self, *args, **kwargs):
-        return self._extract(await self._get_transport().cycle(
-            [self.make_write_gpio(**kwargs)]))
+        return await self.execute(self.make_write_gpio(**kwargs))
 
     def make_read_gpio(self):
         """Return a moteus.Command structure with data necessary to read all
@@ -1111,7 +1128,7 @@ class Controller:
         return result
 
     async def send_diagnostic_write(self, *args, **kwargs):
-        await self._get_transport().cycle([self.make_diagnostic_write(**kwargs)])
+        await self.execute(self.make_diagnostic_write(**kwargs))
 
     def make_diagnostic_read(self, max_length=48, channel=1):
         result = self._make_command(query=True)
@@ -1128,8 +1145,30 @@ class Controller:
         return result
 
     async def diagnostic_read(self, *args, **kwargs):
-        return await self._get_transport().cycle(
-            [self.make_diagnostic_read(**kwargs)])
+        return await self.execute(self.make_diagnostic_read(**kwargs))
+
+    def make_set_trim(self, *, trim=0):
+        result = self._make_command(query=False)
+
+        buf = io.BytesIO()
+        writer = Writer(buf)
+        writer.write_int8(mp.WRITE_INT32 | 0x01)
+        writer.write_varuint(Register.CLOCK_TRIM)
+        writer.write_int32(trim)
+
+        result.data = buf.getvalue()
+        return result
+
+    async def set_trim(self, *args, **kwargs):
+        return await self.execute(self.make_set_trim(*args, **kwargs))
+
+    def _extract(self, value):
+        if len(value):
+            return value[0]
+        return None
+
+    async def execute(self, command):
+        return self._extract(await self._get_transport().cycle([command]))
 
 
 class CommandError(RuntimeError):

fw/BUILD
@@ -97,6 +97,7 @@ MOTEUS_SOURCES = [
     "board_debug.h",
     "board_debug.cc",
     "bootloader.h",
+    "clock_manager.h",
     "drv8323.h",
     "drv8323.cc",
     "error.cc",

fw/clock_manager.h
@@ -1,0 +1,104 @@
+// Copyright 2018-2023 Josh Pieper, jjp@pobox.com.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "mbed.h"
+
+#include "mjlib/micro/async_stream.h"
+#include "mjlib/micro/command_manager.h"
+#include "mjlib/micro/persistent_config.h"
+
+#include "fw/millisecond_timer.h"
+
+namespace moteus {
+
+class ClockManager {
+ public:
+  static constexpr int kMaxExtraTrim = 8;
+
+  ClockManager(MillisecondTimer* timer,
+               mjlib::micro::PersistentConfig& persistent_config,
+               mjlib::micro::CommandManager& command_manager)
+      : timer_(timer) {
+    persistent_config.Register("clock", &clock_, [this]() {
+        this->UpdateConfig();
+      });
+    command_manager.Register("clock", [this](auto&& command, auto&& response) {
+        this->Command(command, response);
+      });
+  }
+
+  void UpdateConfig() {
+    const int32_t trim =
+        std::max<int32_t>(
+            0, std::min<int32_t>(127, clock_.hsitrim + extra_trim_));
+    RCC->ICSCR = (RCC->ICSCR & ~0xff000000) | (trim << 24);
+  }
+
+  void SetTrim(int extra_trim) {
+    extra_trim_ = std::max(-kMaxExtraTrim, std::min(kMaxExtraTrim, extra_trim));
+    UpdateConfig();
+  }
+
+  int trim() const {
+    return extra_trim_;
+  }
+
+  void Command(const std::string_view& command,
+               const mjlib::micro::CommandManager::Response& response) {
+    mjlib::base::Tokenizer tokenizer(command, " ");
+    const auto cmd_text = tokenizer.next();
+
+    if (cmd_text == "us") {
+      snprintf(output_, sizeof(output_), "%" PRIu32 "\r\n",
+               static_cast<uint32_t>(timer_->read_us()));
+      WriteMessage(output_, response);
+    } else if (cmd_text == "trim") {
+      const auto value_str = tokenizer.next();
+
+      if (value_str == "") {
+        snprintf(output_, sizeof(output_), "%d\r\n", extra_trim_);
+        WriteMessage(output_, response);
+      } else {
+        SetTrim(std::strtol(value_str.data(), nullptr, 0));
+        WriteMessage("OK\r\n", response);
+      }
+    } else {
+      WriteMessage("ERR unknown clock\r\n", response);
+    }
+  }
+
+  void WriteMessage(const std::string_view& message,
+                    const mjlib::micro::CommandManager::Response& response) {
+    mjlib::micro::AsyncWrite(*response.stream, message, response.callback);
+  }
+
+ private:
+  struct Config {
+    int32_t hsitrim = 64;
+
+    template <typename Archive>
+    void Serialize(Archive* a) {
+      a->Visit(MJ_NVP(hsitrim));
+    }
+  };
+
+  MillisecondTimer* const timer_;
+  Config clock_;
+  char output_[16] = {};
+  int extra_trim_ = 0;
+};
+
+}

fw/moteus.cc
@@ -27,6 +27,7 @@
 #include "mjlib/multiplex/micro_stream_datagram.h"
 
 #include "fw/board_debug.h"
+#include "fw/clock_manager.h"
 #include "fw/firmware_info.h"
 #include "fw/git_info.h"
 #include "fw/millisecond_timer.h"
@@ -103,59 +104,6 @@ void SetupClock() {
   }
 #endif
 }
-}
-
-namespace {
-class ClockManager {
- public:
-  ClockManager(MillisecondTimer* timer,
-               micro::PersistentConfig& persistent_config,
-               micro::CommandManager& command_manager)
-      : timer_(timer) {
-    persistent_config.Register("clock", &clock_, [this]() {
-        this->UpdateConfig();
-      });
-    command_manager.Register("clock", [this](auto&& command, auto&& response) {
-        this->Command(command, response);
-      });
-  }
-
-  void UpdateConfig() {
-    const int32_t trim = std::max<int32_t>(0, std::min<int32_t>(127, clock_.hsitrim));
-    RCC->ICSCR = (RCC->ICSCR & ~0xff000000) | (trim << 24);
-  }
-
-  void Command(const std::string_view& command,
-               const micro::CommandManager::Response& response) {
-    if (command == "us") {
-      snprintf(output_, sizeof(output_), "%" PRIu32 "\r\n",
-               static_cast<uint32_t>(timer_->read_us()));
-      WriteMessage(output_, response);
-    } else {
-      WriteMessage("ERR unknown clock\r\n", response);
-    }
-  }
-
-  void WriteMessage(const std::string_view& message,
-                    const micro::CommandManager::Response& response) {
-    micro::AsyncWrite(*response.stream, message, response.callback);
-  }
-
- private:
-  struct Config {
-    int32_t hsitrim = 64;
-
-    template <typename Archive>
-    void Serialize(Archive* a) {
-      a->Visit(MJ_NVP(hsitrim));
-    }
-  };
-
-  MillisecondTimer* const timer_;
-  Config clock_;
-  char output_[16] = {};
-};
-
 }
 
 #if defined(TARGET_STM32G4)
@@ -308,6 +256,8 @@ int main(void) {
       &command_manager,
       &telemetry_manager,
       &multiplex_protocol,
+      &clock,
+      &system_info,
       &timer,
       &firmware_info);
 

fw/moteus_controller.cc
@@ -300,6 +300,9 @@ enum class Register {
   kAux2AnalogIn4 = 0x06b,
   kAux2AnalogIn5 = 0x06c,
 
+  kMillisecondCounter = 0x070,
+  kClockTrim = 0x071,
+
   kModelNumber = 0x100,
   kFirmwareVersion = 0x101,
   kRegisterMapVersion = 0x102,
@@ -361,6 +364,8 @@ class MoteusController::Impl : public multiplex::MicroServer::Server {
        micro::CommandManager* command_manager,
        micro::TelemetryManager* telemetry_manager,
        multiplex::MicroServer* multiplex_protocol,
+       ClockManager* clock_manager,
+       SystemInfo* system_info,
        MillisecondTimer* timer,
        FirmwareInfo* firmware)
       : aux1_port_("aux1", "ic_pz1", kAux1PortHardwareConfig,
@@ -418,6 +423,8 @@ class MoteusController::Impl : public multiplex::MicroServer::Server {
 
             return options;
           }()),
+        clock_manager_(clock_manager),
+        system_info_(system_info),
         firmware_(firmware) {}
 
   void Start() {
@@ -576,6 +583,11 @@ class MoteusController::Impl : public multiplex::MicroServer::Server {
         return 0;
       }
 
+      case Register::kClockTrim: {
+        clock_manager_->SetTrim(ReadIntMapping(value));
+        return 0;
+      }
+
       case Register::kSetOutputNearest: {
         const float position = ReadPosition(value);
         bldc_.SetOutputPositionNearest(position);
@@ -633,6 +645,7 @@ class MoteusController::Impl : public multiplex::MicroServer::Server {
       case Register::kAux2AnalogIn3:
       case Register::kAux2AnalogIn4:
       case Register::kAux2AnalogIn5:
+      case Register::kMillisecondCounter:
       case Register::kModelNumber:
       case Register::kSerialNumber1:
       case Register::kSerialNumber2:
@@ -890,6 +903,19 @@ class MoteusController::Impl : public multiplex::MicroServer::Server {
             static_cast<int>(reg) - static_cast<int>(Register::kAux2AnalogIn1);
         return ScalePwm(bldc_.aux2().analog_inputs[pin], type);
       }
+      case Register::kMillisecondCounter: {
+        const uint32_t ms_counter = system_info_->millisecond_counter();
+        switch (type) {
+          case 0: return static_cast<int8_t>(ms_counter % 256);
+          case 1: return static_cast<int16_t>(ms_counter % 65536);
+          case 2: return static_cast<int32_t>(ms_counter);
+          case 3: return static_cast<float>(ms_counter % 8388608);
+        }
+        break;
+      }
+      case Register::kClockTrim: {
+        return IntMapping(clock_manager_->trim(), type);
+      }
 
       case Register::kModelNumber: {
         if (type != 2) { break; }
@@ -942,6 +968,8 @@ class MoteusController::Impl : public multiplex::MicroServer::Server {
   MotorPosition motor_position_;
   Drv8323 drv8323_;
   BldcServo bldc_;
+  ClockManager* const clock_manager_;
+  SystemInfo* const system_info_;
   FirmwareInfo* const firmware_;
 
   bool command_valid_ = false;
@@ -953,10 +981,12 @@ MoteusController::MoteusController(micro::Pool* pool,
                                    micro::CommandManager* command_manager,
                                    micro::TelemetryManager* telemetry_manager,
                                    multiplex::MicroServer* multiplex_protocol,
+                                   ClockManager* clock_manager,
+                                   SystemInfo* system_info,
                                    MillisecondTimer* timer,
                                    FirmwareInfo* firmware)
     : impl_(pool, pool, persistent_config, command_manager, telemetry_manager,
-            multiplex_protocol, timer, firmware) {}
+            multiplex_protocol, clock_manager, system_info, timer, firmware) {}
 
 MoteusController::~MoteusController() {}
 

fw/moteus_controller.h
@@ -19,8 +19,10 @@
 #include "mjlib/multiplex/micro_server.h"
 
 #include "fw/bldc_servo.h"
+#include "fw/clock_manager.h"
 #include "fw/firmware_info.h"
 #include "fw/millisecond_timer.h"
+#include "fw/system_info.h"
 
 namespace moteus {
 
@@ -33,6 +35,8 @@ class MoteusController {
                    mjlib::micro::CommandManager* command_manager,
                    mjlib::micro::TelemetryManager* telemetry_manager,
                    mjlib::multiplex::MicroServer* multiplex_protocol,
+                   ClockManager*,
+                   SystemInfo*,
                    MillisecondTimer*,
                    FirmwareInfo*);
   ~MoteusController();

fw/system_info.cc
@@ -32,7 +32,11 @@ struct SystemInfoData {
 
   uint32_t idle_rate = 0;
   uint32_t can_reset_count = 0;
-  uint32_t ms_count = 0;
+
+  // We deliberately start this counter near to int32 overflow so that
+  // any applications that use it will likely have to handle it
+  // properly.
+  uint32_t ms_count = (1ull<<31) - 300000;
 
   template <typename Archive>
   void Serialize(Archive* a) {
@@ -95,6 +99,10 @@ void SystemInfo::PollMillisecond() {
 
 void SystemInfo::SetCanResetCount(uint32_t value) {
   impl_->SetCanResetCount(value);
+}
+
+uint32_t SystemInfo::millisecond_counter() const {
+  return impl_->data_.ms_count;
 }
 
 }

fw/system_info.h
@@ -30,6 +30,8 @@ class SystemInfo {
   void PollMillisecond();
   void SetCanResetCount(uint32_t);
 
+  uint32_t millisecond_counter() const;
+
   // Increment this from an idle thread.
   static volatile uint32_t idle_count;
 

commit 5090d56fb1f20acc0c5562656897b654519a2c60

Fix moteus_tool operations
0d76d71b introduced a regression by assuming that all of the "set_"
python APIs behaved the same, when in fact the diagnostic ones
returned all possible responses in order to be more robust to
communication problems.

lib/python/moteus/moteus.py
@@ -1128,7 +1128,7 @@ class Controller:
         return result
 
     async def send_diagnostic_write(self, *args, **kwargs):
-        await self.execute(self.make_diagnostic_write(**kwargs))
+        await self._get_transport().cycle([self.make_diagnostic_write(**kwargs)])
 
     def make_diagnostic_read(self, max_length=48, channel=1):
         result = self._make_command(query=True)
@@ -1145,7 +1145,8 @@ class Controller:
         return result
 
     async def diagnostic_read(self, *args, **kwargs):
-        return await self.execute(self.make_diagnostic_read(**kwargs))
+        return await self._get_transport().cycle(
+            [self.make_diagnostic_read(**kwargs)])
 
     def make_set_trim(self, *, trim=0):
         result = self._make_command(query=False)