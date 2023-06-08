a52442aca97a04590891bbcf6d8550ae9d11c39e
Josh Pieper <jjp@pobox.com>
2023-06-08 11:14:22 -0400

    Update program_option_bytes to actually exit

--- fw/program_option_bytes.sh
+++ fw/program_option_bytes.sh
@@ -8,5 +8,8 @@ openocd \
     -f interface/stlink.cfg \
     -f target/stm32g4x.cfg \
     -c "init" \
+    -c "reset halt" \
     -c "stm32l4x option_write 0 0x20 0x00000000 0x04000000" \
-    -c "stm32l4x option_load 0"
+    -c "stm32l4x option_load 0" \
+    -c "reset" \
+    -c "exit"
